755b701c669b4f7fd5fd06bd1a1be22fc4ebf5f4
Nitin <pmitwithnitin@gmail.com>
2025-08-30 17:51:52 +0530

    remove types for node

--- apps/chat/vite.config.ts
+++ apps/chat/vite.config.ts
@@ -1,23 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
 	// Loads React plugin for fast-refresh and JSX/TSX handling; enables targeted HMR updates for React components.
-	plugins: [react(), tsconfigPaths({ root: "./" })],
+	plugins: [react(), tsconfigPaths()],
 	// Prefixes all asset URLs with '/apps/chat/'
 	base: "/apps/chat/",
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-			"@Providers": path.resolve(__dirname, "./src/Providers"),
-			"@Reductions": path.resolve(__dirname, "./src/Reductions"),
-			"@Pages": path.resolve(__dirname, "./src/Pages"),
-			"@Router": path.resolve(__dirname, "./src/Router"),
-		},
-	},
 	server: {
 		// Restricts dev server to accept requests only from 'preplaced.schrodingers.cat' via NGINX proxy for security.
 		allowedHosts: ["preplaced.schrodingers.cat"],
